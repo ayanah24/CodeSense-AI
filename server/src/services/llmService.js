633f042b56e2b9570fc3898f59e9b0024bad10e7
ayanah24 <ayan27452@gmail.com>
2026-06-05 16:50:48 +0530

    test ai review

--- server/src/services/llmService.js
+++ server/src/services/llmService.js
@@ -62,7 +62,7 @@ async function getCodeReview(prTitle, author, formattedDiff) {
 
   try {
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         contents: [
           {
